3309596ac41b2368ecb93bc86bd738661f589003
Mr. Python <mrpython@mrpython.top>
2026-01-22 16:47:49 +0000

    原来是gitignore写错了

--- examples/CSP-S2024/meta.ts
+++
@@ -1,7 +0,0 @@
-import type ExampleMetaExport from "@/types/examples";
-
-export default {
-  displayName: "CSP-S2024",
-  title: "2024 CCF 非专业级软件能力认证",
-  template: "cnoi",
-} satisfies ExampleMetaExport;
---
+++ examples/CSP-S2024/meta.tsx
@@ -0,0 +1,18 @@
+import type ExampleMetaExport from "@/types/examples";
+
+export default {
+  displayName: "CSP-S2024",
+  title: "2024 CCF 非专业级软件能力认证",
+  template: "cnoi",
+  description: (
+    <>
+      原始 PDF：
+      <a
+        href="https://qoj.ac/download.php?type=attachments&id=1848&r=0"
+        target="_blank"
+      >
+        https://qoj.ac/download.php?type=attachments&id=1848&r=0
+      </a>
+    </>
+  ),
+} satisfies ExampleMetaExport;
